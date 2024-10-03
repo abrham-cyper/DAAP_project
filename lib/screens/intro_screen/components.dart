21f626c570eaf5d2b28d0e660fecf28c1ed7166d
abrham <abrhamassefa759@gamil.com>
2024-10-03 16:32:16 +0300

    splashe screen

--- lib/screens/intro_screen/components.dart
+++ lib/screens/intro_screen/components.dart
@@ -1,95 +1,38 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import '../../../utils/colors.dart';
-import '../home/home_screen.dart';
-import '../intro_screen/Login.dart';
+import 'Login.dart'; // Ensure this points to your home_page.dart
 
-class GetStartBtn extends StatefulWidget {
-  const GetStartBtn({
-    Key? key,
-    required this.size,
-    required this.textTheme,
-  }) : super(key: key);
-  final Size size;
-  final TextTheme textTheme;
+class IntoScreen extends StatefulWidget {
+  const IntoScreen({Key? key}) : super(key: key);
+
   @override
-  State<GetStartBtn> createState() => _GetStartBtnState();
+  State<IntoScreen> createState() => _IntoScreenState();
 }
 
-class _GetStartBtnState extends State<GetStartBtn> {
-  bool isLoading = false;
-  loadingHandler() {
-    setState(() {
-      isLoading = true;
-      Future.delayed(const Duration(seconds: 2)).then((value) {
-        isLoading = false;
-        Navigator.pushReplacement(
-            context, CupertinoPageRoute(builder: (_) => LoginPage()));
-      });
+class _IntoScreenState extends State<IntoScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Navigate to home page after a delay
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (context) =>
+                LoginPage()), // Adjust this to your actual HomePage class
+      );
     });
   }
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: loadingHandler,
-      child: Container(
-        margin: const EdgeInsets.only(top: 60),
-        width: widget.size.width / 1.5,
-        height: widget.size.height / 13,
-        decoration: BoxDecoration(
-            color: MyColors.btnColor, borderRadius: BorderRadius.circular(15)),
-        child: Center(
-            child: isLoading
-                ? const Center(
-                    child: SizedBox(
-                      width: 30,
-                      height: 30,
-                      child: CircularProgressIndicator(
-                        color: Colors.white,
-                      ),
-                    ),
-                  )
-                : Text(
-                    "Track It Here",
-                    style: widget.textTheme.headlineMedium?.copyWith(
-                      color: Colors.white,
-                    ),
-                  )),
-      ),
-    );
-  }
-}
-
-class SkipBtn extends StatelessWidget {
-  const SkipBtn({
-    Key? key,
-    required this.size,
-    required this.textTheme,
-    required this.onTap,
-  }) : super(key: key);
-
-  final Size size;
-  final TextTheme textTheme;
-  final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(top: 60),
-      width: size.width / 1.5,
-      height: size.height / 13,
-      decoration: BoxDecoration(
-          border: Border.all(
-            color: MyColors.btnBorderColor,
-            width: 2,
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Hello World',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
           ),
-          borderRadius: BorderRadius.circular(15)),
-      child: InkWell(
-        borderRadius: BorderRadius.circular(15.0),
-        onTap: onTap,
-        splashColor: MyColors.btnBorderColor,
-        child: Center(
-          child: Text("Skip", style: textTheme.displaySmall),
         ),
       ),
     );
